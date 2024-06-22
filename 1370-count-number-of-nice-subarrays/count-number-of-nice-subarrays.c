dea55f9ae9d4481b6948df4284e8a90993566c9b
Sayar Basu <sayar.basu.cse26@heritageit.edu.in>
2024-06-22 17:17:44 +0530

    Time: 97 ms (20.29%) | Memory: 12.9 MB (6.52%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.c
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.c
@@ -1,13 +1,11 @@
 int isOdd(int num) {
     return num % 2 != 0;
 }
-
 int numberOfSubarrays(int* nums, int numsSize, int k) {
     int count = 0;
     int oddCount = 0;
     int* oddCounter = (int*)calloc(numsSize + 1, sizeof(int));
-    oddCounter[0] = 1;  // Base case for 0 odd numbers encountered initially
-
+    oddCounter[0] = 1;
     for (int i = 0; i < numsSize; i++) {
         if (isOdd(nums[i])) {
             oddCount++;
@@ -17,7 +15,6 @@ int numberOfSubarrays(int* nums, int numsSize, int k) {
         }
         oddCounter[oddCount]++;
     }
-
     free(oddCounter);
     return count;
 }
