453f034f16916b29f5f2189a483984f5685cc90e
Sayar Basu <sayar.basu.cse26@heritageit.edu.in>
2024-07-06 14:33:19 +0530

    Time: 0 ms (100.00%) | Memory: 5.3 MB (65.63%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.c
+++ 2645-pass-the-pillow/pass-the-pillow.c
@@ -3,8 +3,8 @@ int passThePillow(int n, int time) {
     int direction = 1; 
     for (int i = 0; i < time; ++i) {
         position += direction;
-        if (position == n - 1 || position == 0) // Change direction at ends
+        if (position == n - 1 || position == 0) 
             direction = -direction;
     }
-    return position + 1; //
+    return position + 1; 
 }
