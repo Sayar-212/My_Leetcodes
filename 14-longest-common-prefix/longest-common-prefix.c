877ce5a54092d55a611f489bc6707fcd7c1065c3
Sayar Basu <sayar.basu.cse26@heritageit.edu.in>
2024-07-10 02:51:11 +0530

    Time: 0 ms (100.00%) | Memory: 5.9 MB (11.43%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.c
@@ -0,0 +1,19 @@
+char * longestCommonPrefix(char ** strs, int strsSize){
+    for(int i = 0; i < strlen(*strs); i++)
+    {
+        for(int j = 1; j < strsSize; j++)
+        {
+            if(*(strs[0]+i) == *(strs[j]+i))
+            {
+                
+            }
+            else
+            {
+                char *temp = (strs[0]+i);
+                *temp = '\0';
+                return *strs;
+            }
+        }
+    }
+    return *strs;
+}
